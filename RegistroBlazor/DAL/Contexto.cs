a655d188f07a28aff428f8891d0e880c230de83b
Rehani Martin Cordero Tejada <rehani_cordero@ucne.edu.do>
2020-05-20 21:03:45 -0400

    Agregando componentes..

---
+++ RegistroBlazor/DAL/Contexto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RegistroBlazor.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroBlazor.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Notas> Personas { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source=DATA\PersonasControl.db");
+        }
+
+    }
+}
---
+++ RegistroBlazor/Models/Notas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RegistroBlazor.Models
+{
+    public class Notas
+    {
+        [Key]
+        [Required(ErrorMessage = "El campo Id no puede estar vacío.")]
+        [Range(0, 1000000, ErrorMessage = "El campo Id no puede ser menor que cero o mayor a 1000000.")]
+        public int notasId { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required(ErrorMessage = "El campo fecha no puede estar vacío.")]
+        [DisplayFormat(DataFormatString = "{0:dd,mm, yyyy}")]
+        public DateTime fecha { get; set; }
+        [Required(ErrorMessage = "El campo RUTA es requerido.")]
+        public string ruta { get; set; }
+        [Range(0, 10, ErrorMessage = "La importancia debe ser de 0 a 10.")]
+        public int importancia { get; set; }
+
+    }
+}
